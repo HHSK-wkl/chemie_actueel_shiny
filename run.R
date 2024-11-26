25777e7d2e7bf6a260673f5b584f9147412f26d5
Johan van Tent <tentvanjohan@hotmail.com>
2024-11-26 12:41:54 +0100

    niet meer de quarto versie

--- run.R
+++ run.R
@@ -1,4 +1,5 @@
 library(shiny)
 setwd("C:/R/chemie_actueel_shiny")
-quarto::quarto_run("chemie_actueel.qmd")
-# runApp(launch.browser = TRUE)
+# quarto::quarto_run("chemie_actueel.qmd")
+# quarto::quarto_serve("chemie_actueel.qmd", render = FALSE)
+runApp(launch.browser = TRUE)
