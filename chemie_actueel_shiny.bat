870815e3b35c7cf39a53841696a5b7330c3a6086
Johan van Tent <tentvanjohan@hotmail.com>
2025-07-28 10:46:08 +0200

    R 4.4.1

--- chemie_actueel_shiny.bat
+++ chemie_actueel_shiny.bat
@@ -1,1 +1,1 @@
-"C:/Program Files/R/R-4.3.0/bin/x64/Rscript.exe" -e "source('C:/R/chemie_actueel_shiny/run.R')"
+"C:/Program Files/R/R-4.4.1/bin/x64/Rscript.exe" -e "source('C:/R/chemie_actueel_shiny/run.R')"
